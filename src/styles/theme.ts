2d5e542992faefa4bc8eeef3565d1d66e3f0ba9b
André Navarro <andre_farao123@hotmail.com>
2022-09-16 19:38:53 -0300

    Styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    background_primary: '#eeeeee',
+    background_secondary: '#ffffff',
+
+    text: '#1b1b1f',
+    text_detail: '#aeaeb3',
+    url: '#0645ad',
+
+    line: '#ebebf0',
+
+    shape: '#e1e1e8',
+    shape_dark: '#29292e',
+
+    g: '#00ff00',
+    pg: '#ffff00',
+    'pg-13': '#ff9900',
+    r: '#ff0000',
+  },
+  fonts: {
+    primary_400: 'DMSans_400Regular',
+    primary_700: 'DMSans_700Bold',
+  },
+};
